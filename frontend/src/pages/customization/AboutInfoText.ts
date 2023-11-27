42ea604895e8fef35ee2152f34d1f26ebdada066
Najmul11 <najmulbinnurul@gmail.com>
2023-11-27 14:24:04 +0600

    changed resume link

--- frontend/src/pages/customization/AboutInfoText.ts
+++ frontend/src/pages/customization/AboutInfoText.ts
@@ -5,12 +5,12 @@ export const introTag =
   "I'm a full stack developer based in Dhaka, Bangladesh,with proficiency in a wide range of modern web technologies.";
 
 export const github = "https://github.com/Najmul11";
-export const linkedIn = "https://www.linkedin.com/in/nazpro/";
+export const linkedIn = "https://www.linkedin.com/in/najmulhoque-/";
 export const whatsapp = "https://api.whatsapp.com/send?phone=8801886500072";
 export const discord = "https://discord.com/channels/@slise.web3";
 export const mail = "mailto:$najmulbinnurul@gmail.com";
 
 export const resume =
-  "https://drive.google.com/file/d/14Qeyv0n8JjN1_P4K7tidUP10OQccdY0b/view?usp=drive_link";
+  "https://drive.google.com/file/d/1HPDKl35DAokYwtJdwsj4w3vWVDZMbB2d/view?usp=drive_link";
 
 export const lastUpdate = "Last Updated on: 16-Sep-2023";
